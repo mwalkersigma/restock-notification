79349922f226d43934ba72f6d396feb8f83821cd
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2024-09-30 13:55:37 -0500

    feat(config): Adding config file

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 const {db} = require('./modules/db');
 const {PromisePool} = require('@supercharge/promise-pool')
-const {subDays, addHours} = require('date-fns')
+const {subDays, addHours, addDays, setHours, setMinutes, setSeconds, setMilliseconds} = require('date-fns')
 const {sendRingCentralMessage} = require("./modules/ringCentral");
 const {formatter} = require("./modules/utils/numberFormatter");
+const fs = require('fs/promises')
 
+async function getConfig() {
+    try {
+        return JSON.parse(await fs.readFile('./config.json', 'utf-8'))
+    } catch (e) {
+        console.error(e)
+        throw new Error('Error reading config file')
+    }
+}
+
+async function setConfig(config) {
+    return await fs.writeFile('./config.json', JSON.stringify(config, null, 2))
+}
 
 async function getPickTransactions(startDate, endDate) {
     if (!startDate || !endDate) {
@@ -21,10 +34,16 @@ async function getPickTransactions(startDate, endDate) {
         console.log("No End Date Provided, defaulting to today")
         endDate = new Date()
     }
-    console.log(`Querying Pick Transactions in the date range: ${startDate} - ${endDate}`)
+    console.log(`Querying Pick Transactions for: ${startDate.toString()} upto ${endDate.toString()}`)
+    startDate = startDate.toISOString().split('T')[0]
+    endDate = endDate.toISOString().split('T')[0]
 
     return await db.query(`
-        SELECT t.*
+        SELECT transaction_date,
+               sku,
+               quantity,
+               quantity_before,
+               quantity_after
         FROM surtrics.surplus_metrics_data t
         WHERE transaction_type = 'Pick'
           AND sku LIKE '%-3'
@@ -46,24 +65,20 @@ async function getUsedComponentQuantity(sku) {
     if (!sku.includes('-3')) {
         throw new Error('Invalid SKU format')
     }
-    const refurbComponent = await db.query(`
-        SELECT t.*
+    let baseQuery = `
+        SELECT retail_price,
+               quantity,
+               sku
         FROM sursuite.components t
         WHERE sku = $1
-    `, [sku]).then(res => res.rows[0])
+    `
+    const refurbComponent = await db.query(baseQuery, [sku]).then(res => res.rows[0])
     if (!refurbComponent) {
         throw new Error('Refurbished Component not found')
     }
-    console.log(refurbComponent['retail_price'])
     sku = sku.replace('-3', '-4')
     console.log(`Querying Used Component Quantity for SKU: ${sku}`)
-
-    const usedComponent = await db.query(`
-        SELECT t.*
-        FROM sursuite.components t
-        WHERE sku = $1
-    `, [sku])
-        .then(res => res.rows[0])
+    const usedComponent = await db.query(baseQuery, [sku]).then(res => res.rows[0])
         .catch(err => {
             console.error(err)
             throw new Error('Error querying the database')
@@ -93,18 +108,13 @@ async function getUsedComponents(pickTransactions) {
     return results
 }
 
-async function getItemsNeedingRestock(usedComponents) {
+async function getItemsToRefurbFrom(usedComponents) {
     return usedComponents
         .filter(item => item['quantity'] > 0)
 }
 
 async function generateRestockNotification(baseNotification, itemsNeedingRestockNotification) {
     let restockMessage = baseNotification
-
-    if (itemsNeedingRestockNotification.length === 0) {
-        restockMessage += `No Items found that can be refurbished with used inventory. ( We sold all of our refurbished inventory and have no used inventory to refurbish )`
-        return restockMessage
-    }
     restockMessage += `*The following items had all on hand quantity in refurbished condition picked, These items have Used inventory available to be refurbished.* \n`
     restockMessage += `---\n`
     restockMessage += itemsNeedingRestockNotification.map(generateSkuRestockMessage).join('\n')
@@ -116,9 +126,9 @@ async function recordNotification(itemsNeedingRestockNotification) {
     const {results} = await PromisePool
         .for(itemsNeedingRestockNotification)
         .withConcurrency(10)
-        .process(async (item) =>{
+        .process(async (item) => {
             return await db.query(`
-                INSERT INTO nfs.surtrics.restock_notifications ( sku, refurbished_price, used_price ) 
+                INSERT INTO nfs.surtrics.restock_notifications (sku, refurbished_price, used_price)
                 VALUES ($1, $2, $3)
             `, [item['sku'], item['refurbishedRetailPrice'], item['retail_price']])
         });
@@ -126,48 +136,96 @@ async function recordNotification(itemsNeedingRestockNotification) {
 }
 
 (async () => {
-    let message;
+    let message, config, startDate;
     try {
-        const endDate = addHours(new Date('2024-09-19'), 5)
-        const startDate = subDays(endDate, 1)
-        let baseNotification = `** Restock Notification For : ${startDate.toDateString()} ** \n`
-        const pickTransactions = await getPickTransactions(startDate, endDate)
-
+        config = await getConfig()
+        if (!config) {
+            config = {
+                debug: false,
+                dryRun: false,
+            }
+            return new Error("No Config Found")
+        }
+        if(!config['lastRun']){
+            let temp = new Date();
+            temp = subDays(temp,1)
+            temp = setHours(temp,0)
+            temp = setMinutes(temp,0)
+            temp = setSeconds(temp,0)
+            temp = setMilliseconds(temp,0)
+            config['lastRun'] = temp.toString()
+            console.log("No Last Run Date Found, defaulting to yesterday")
+        }
+        startDate = new Date(config['lastRun'])
+        console.log(`Last Run: ${startDate.toString()}`)
+        let queryStartDate = startDate
+        const endDate = addDays(startDate,1)
+        let baseNotification = `** ${config['baseMessage']} ${startDate.toDateString()} - ${endDate.toDateString()} ** \n`
+        const pickTransactions = await getPickTransactions(queryStartDate, endDate)
+        console.log("Updating Last Run Date")
+        config['lastRun'] = endDate.toString();
+        await setConfig(config)
+        console.log("Last Run Date Updated")
+        if (config['debug']) {
+            console.log("Pick Transactions")
+            console.table(pickTransactions)
+        }
         if (pickTransactions.length === 0) {
-            console.log('No Pick Transactions found in the date range')
-            message = baseNotification + 'No Items Requiring Refurbishment Found'
-            await sendRingCentralMessage(message)
+            console.log('No Pick Transactions matching query found in the date range')
+            message = baseNotification + config['ErrorNoItems']
+            if (!config['debug'] && !config['dryRun']) {
+                await sendRingCentralMessage(message)
+            }
             return
         }
-        const usedComponents = await getUsedComponents(pickTransactions);
-        if (usedComponents.length === 0) {
-            console.log('No Used Components found in the date range')
-            message = baseNotification + 'No Items Requiring Refurbishment Found'
-            await sendRingCentralMessage(message)
-            return
-        }
-        const itemsNeedingRestock = await getItemsNeedingRestock(usedComponents);
-        if (itemsNeedingRestock.length === 0) {
-            console.log('No Items needing restock found in the date range')
-            message = baseNotification + 'No Items Requiring Refurbishment Found'
-            await sendRingCentralMessage(message)
-            return
-        }
-        console.log("Found Items needing restock");
-        console.table(itemsNeedingRestock)
-        console.log("Recording Notification to Database")
-        await recordNotification(itemsNeedingRestock)
-        console.log("Generating Restock Notification")
-        message = await generateRestockNotification(baseNotification, itemsNeedingRestock)
-        console.log("Sending Notification")
-        console.log(message)
-        await sendRingCentralMessage(message)
-        console.log("Notification Sent")
 
+        const usedComponents = await getUsedComponents(pickTransactions);
+        if (config['debug']) {
+            console.log("Used Components")
+            console.table(usedComponents)
+        }
+        if (usedComponents.length === 0) {
+            console.log('No Used Components found')
+            message = baseNotification + config['ErrorNoUsedItems']
+            if (!config['debug'] && !config['dryRun']) {
+                await sendRingCentralMessage(message)
+            }
+            return
+        }
+
+        const itemsForRestock = await getItemsToRefurbFrom(usedComponents);
+        if (config['debug']) {
+            console.log("Items for restock")
+            console.table(itemsForRestock)
+        }
+        if (itemsForRestock.length === 0) {
+            console.log('No Items to restock from')
+            message = baseNotification + config['ErrorNoItemsInStock']
+            if (!config['debug'] && !config['dryRun']) {
+                await sendRingCentralMessage(message)
+            }
+            return
+        }
+        console.log(config['MessageFoundItems'])
+        message = await generateRestockNotification(baseNotification, itemsForRestock)
+
+        if (config['debug']) {
+            console.log(message)
+        }
+        if (!config['debug'] && !config['dryRun']) {
+            console.log("Recording Notification")
+            await recordNotification(itemsForRestock)
+            console.log("Sending Notification")
+            await sendRingCentralMessage(message)
+            console.log("Sending Notification")
+        }
+        console.log("Notification Sent")
     } catch (error) {
         console.error(error)
         try {
-            await sendRingCentralMessage(`**Error in Restock Notification** \n ${error.message}`)
+            if (!config?.['debug'] && !config?.['dryRun']) {
+                await sendRingCentralMessage(`**Error in Restock Notification** \n ${error.message}`)
+            }
         } catch (e) {
             console.log("Error could not be sent to RingCentral")
             console.error(e)
